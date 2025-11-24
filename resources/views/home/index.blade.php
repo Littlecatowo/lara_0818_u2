b140ad7affc23d687b515fa611080376f125e3f1
Littlecatowo <tobytw3@gmail.com>
2025-11-24 23:08:44 +0800

    changing index.php to index.blade.php

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="zh_tw">
+    @extends('layouts.master')
+    @section('title', 'Home')
+    @section('content')
+        <h1>Home</h1>
+        <p>This is my body content.</p>
+    @endsection
+</html>
